b5f595bdd7742d715014d37d08f603384f58db90
maxbalykin <void123void@github.com>
2026-06-05 10:31:44 +0300

    Первый коммит

---
+++ 03.06.Lesson/src/edu/training/jc/simple_output/TableOutput.java
@@ -0,0 +1,13 @@
+package edu.training.jc.simple_output;
+
+public class TableOutput {
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------");
+        System.out.println("|\t\tid\t\t|\t\trole\t|");
+        System.out.println("---------------------------------");
+        System.out.println("|\t\t1\t\t|\t\tguest\t|");
+        System.out.println("|\t\t10\t\t|\t\tuser\t|");
+        System.out.println("|\t\t100\t\t|\t\tadmin\t|");
+        System.out.println("---------------------------------");
+    }}
